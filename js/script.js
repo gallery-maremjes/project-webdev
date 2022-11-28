e30ac47983e0fabefc1d4ce634beac26926b0f18
x22117679ncirl <116448725+x22117679ncirl@users.noreply.github.com>
2022-11-28 12:39:15 +0000

    Commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
+/* Mario Petrizzelli
+Group Project */
+
+/* Creating of global variables to be used across all functions */
+/* Documentation: https://www.w3schools.com/jsref/met_document_queryselector.asp */
+const today = new Date;
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const items = document.querySelectorAll(".item");
+
+/* Get date */
+function date() {
+    /* Get date information from Date. Documentation: https://www.w3schools.com/js/js_date_methods.asp */
+    var date = today.getDate();
+    var dayIndex = today.getDay();
+    var monthIndex = today.getMonth();
+    var year = today.getFullYear();
+
+    const daysOuput = ["Sunday", "Monday", "Tuesday", "Wedensday", "Thursday", "Friday", "Saturday"];
+    const monthsOutput = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    document.getElementById("date-bar").innerHTML = "<i class='fas fa-calendar'></i> &nbsp" + daysOuput[dayIndex] + ", " + date + " " + monthsOutput[monthIndex] + " " + year + " | Dublin, Ireland";
+}
 
 /* Toggle mobile menu */
 function toggleMenu() {
